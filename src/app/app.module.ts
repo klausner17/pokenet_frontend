97a6bc58b22769bd19a876a1de9cae7a0ea33551
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-16 13:42:20 -0200

    home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { LoginModule } from './login/login.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
