77dca0dacc39c1d8464706d659a12cc4c8de5b4b
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-21 17:24:32 -0200

    utilizando form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -24,7 +25,8 @@ import { ProfileService } from './profile/profile.service';
     BrowserModule,
     MaterializeModule,
     LoginModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     AuthService,
