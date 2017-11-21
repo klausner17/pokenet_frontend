dd6544ff12a998863910f0010cd6da3ca9b70c58
Klausner Augusto <klausner_17@yahoo.com.br>
2017-11-21 20:00:57 -0200

    movendo raid para organizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { LoginModule } from './login/login.module';
 import { HomeComponent } from './home/home.component';
-import { RaidComponent } from './raid/raid.component';
+import { RaidComponent } from './raid/raid/raid.component';
 import { AuthService } from './auth.service';
 import { AuthGuardService } from './guards/auth-guard.service';
 import { ProfileComponent } from './profile/profile/profile.component';
