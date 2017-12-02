13b80531c926604bbb37fa89db51da1cdc2d3fc2
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-12-02 02:53:13 -0200

    editando o package config para o heroku reconhecer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const port = process.env.PORT || 3001;
