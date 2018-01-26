c06d5f5a70fb1f9c0ad3279b9e9beb7a5b09775a
Klausner Augusto <klausner_17@yahoo.com.br>
2018-01-26 14:07:42 -0200

    configurando testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,52 @@
+import { KeyResolverGuard } from './guards/key.resolver.guard';
+import { ProfileResolverGuard } from './guards/profile.resolver.guard';
+import { PokemonGymResolverGuard } from './guards/pokemonGym.resolver.guard';
+import { GymResolverGuard } from './guards/gym.resolver.guard';
+import { TrainnersResolverGuard } from './guards/trainners.resolver.guard';
+import { HomeResolverGuard } from './guards/home.resolver.guard';
+import { RaidResolverGuard } from './raid/raid.resolver.guard';
+import { RaidService } from './raid/raid.service';
+import { ProfileService } from './profile/profile.service';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginService } from './login/login.service';
+import { BlockUIModule } from 'ng-block-ui';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing';
+import { MaterializeModule } from 'angular2-materialize';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthService } from './auth.service';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        LoginService,
+        AuthService,
+        AuthGuard,
+        ProfileService,
+        RaidService,
+        RaidResolverGuard,
+        HomeResolverGuard,
+        TrainnersResolverGuard,
+        GymResolverGuard,
+        PokemonGymResolverGuard,
+        ProfileResolverGuard,
+        LoginService,
+        KeyResolverGuard,
+        RouterModule
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA,
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
